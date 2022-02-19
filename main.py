46b77b2657019dce284e6037b02b22374bace7d4
dyrtage <dyrtage@gmail.com>
2022-02-19 12:39:31 +0300

    Готовый вариант отправки рандомной гачи фразы выбранному пользователю

--- main.py
+++ main.py
@@ -6,19 +6,22 @@ from telethon import TelegramClient
 
 # Начинаем работу клиента
 async def start_client():
+
+    # Более удобный подход к регистрации своего клиента
     async with TelegramClient('session_name', config.API_ID, config.API_HASH) as client:
+        message = ""  # Пустая переменная для фразы
+
         # Загружаем фразы из текстового файла
         file = open('phrases.txt', encoding="utf8")
-        message = ""
 
         # Заносим все фразы в список
         gachi_phrases = list(set(file))  # Очистка фраз от мусора
-        file.close()
+        file.close()  # Закроем навсегда текстовый файл
 
         # Отправляем сообщения нашему челу
-        while len(message) < 40:
-            message = random.choice(gachi_phrases)  # Случайная фраза
-            await client.send_message('C_h_e_l_l', message)  # Отправка выбранному пользователю
+        while len(message) < 60:  # Фильтруем мусор по размерам фразы
+            message = random.choice(gachi_phrases)  # Выбираем случайную фраза
+            await client.send_message(input("Введите имя пользователя: "), message)  # Отправка выбранному пользователю
 
 
-asyncio.run(start_client())
+asyncio.run(start_client())  # Запуск
